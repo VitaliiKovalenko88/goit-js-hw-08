8c8824cef75e32609885dd41c200ff9f0507ba62
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-11-05 03:13:15 +0200

    сделал 2 задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME_PLAYER = 'videoplayer-current-time';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+  localStorage.setItem(CURRENT_TIME_PLAYER, data.seconds);
+};
+
+const setTime = () => {
+  player
+    .setCurrentTime(localStorage.getItem(CURRENT_TIME_PLAYER))
+    .then(function (seconds) {
+      // seconds = the actual time that the player seeked to
+    })
+    .catch(function (error) {
+      switch (error.name) {
+        case 'RangeError':
+          // the time was less than 0 or greater than the video’s duration
+          break;
+
+        default:
+          // some other error occurred
+          break;
+      }
+    });
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+window.addEventListener('DomContentLoaded', setTime);
