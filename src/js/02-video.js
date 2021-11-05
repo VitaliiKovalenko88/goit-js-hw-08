73d833128c787e08515368d6697a63de91f0986a
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-11-05 03:28:25 +0200

    Удалил обработчик собітий с Windo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -10,24 +10,21 @@ const onPlay = function (data) {
   localStorage.setItem(CURRENT_TIME_PLAYER, data.seconds);
 };
 
-const setTime = () => {
-  player
-    .setCurrentTime(localStorage.getItem(CURRENT_TIME_PLAYER))
-    .then(function (seconds) {
-      // seconds = the actual time that the player seeked to
-    })
-    .catch(function (error) {
-      switch (error.name) {
-        case 'RangeError':
-          // the time was less than 0 or greater than the video’s duration
-          break;
+player
+  .setCurrentTime(localStorage.getItem(CURRENT_TIME_PLAYER))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-        default:
-          // some other error occurred
-          break;
-      }
-    });
-};
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
 player.on('timeupdate', throttle(onPlay, 1000));
-window.addEventListener('DomContentLoaded', setTime);
