a3c327d1dd79550bc7d459655b244fa21f40f4f2
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-11-07 09:05:42 +0200

    чуть изменил

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,7 +2,6 @@ import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const CURRENT_TIME_PLAYER = 'videoplayer-current-time';
-
 const iframe = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(iframe);
 
@@ -10,21 +9,11 @@ const onPlay = function (data) {
   localStorage.setItem(CURRENT_TIME_PLAYER, data.seconds);
 };
 
-player
-  .setCurrentTime(localStorage.getItem(CURRENT_TIME_PLAYER))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
-  .catch(function (error) {
-    switch (error.name) {
-      case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
-        break;
-
-      default:
-        // some other error occurred
-        break;
-    }
-  });
-
+const saveTime = () => {
+  const seconds = localStorage.getItem(CURRENT_TIME_PLAYER);
+  if (seconds) {
+    player.setCurrentTime(seconds);
+  }
+};
 player.on('timeupdate', throttle(onPlay, 1000));
+window.addEventListener('DOMContentLoaded', saveTime);
